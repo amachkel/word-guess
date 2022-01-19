d9d9bdbf8a162c1567bcbc4ccca6c1c040dddeb7
amachkel <amachkel@gmail.com>
2022-01-19 11:38:41 -0500

    Add createSpan(letter) and set attributes, then appendChild to #word. Put for loop in newRound() to run createSpan() for each letter in random word

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,58 @@
 var timeEl = document.querySelector(".time");
 var countDown = 10;
 var wordArray = ["kitty", "doggo", "parrot", "gecko"];
-var randomWord = Math.floor(Math.random() * wordArray.length);
+var randomWord = getRandomWord();
+// console.log(randomWord);
+var splitWord = randomWord.split("");
 var startButton = document.querySelector("#start-button");
 //sets game's default to off;
 var startRound = "off";
-function newRound() {
-  var timerInterval = setInterval(function () {
-    countDown--;
-    timeEl.textContent = "Countdown: " + countDown;
-
-    for (i = 0; i < randomWord; i++);
-
-    if (countDown === 0 || startRound === "off") {
-      clearInterval(timerInterval);
-      sendMessage();
-    }
-  }, 1000);
-}
-
-document.getElementById("word").textContent = wordArray[randomWord];
 
 startButton.addEventListener("click", function () {
   if (startRound === "off") {
-      startRound = "on";
-      newRound();
+    startRound = "on";
+    newRound();
   } else {
-      startRound = "off";
+    startRound = "off";
   }
-  
 });
 
 function sendMessage() {
   timeEl.textContent = "Game Over";
+}
+
+function newRound() {
+  var timerInterval = setInterval(function () {
+    countDown--;
+    timeEl.textContent = "Countdown: " + countDown;
+    //if time runs out, or btn is clicked during round, timer clears & msg displays
+    if (countDown === 0 || startRound === "off") {
+      clearInterval(timerInterval);
+      sendMessage();
+      //need to be able to start new round when countDown = 0.
+    }
+  }, 1000);
+
+  for (i = 0; i < splitWord.length; i++) {
+    createSpan(splitWord[i]);
+  }
+}
+
+function getRandomWord() {
+  var random = Math.floor(Math.random() * wordArray.length);
+  return wordArray[random];
+}
+//letter paramater = any letter
+function createSpan(letter) {
+    //creates <span> tag
+  var span = document.createElement("span");
+  //<span class="letter-holder" data-letter-val= letter>
+  span.setAttribute("class", "letter-holder");
+  span.setAttribute("data-letter-val", letter);
+  //<span> text reads "_ ". 
+  span.textContent = "_ ";
+  var wordDiv = document.getElementById("word");
+  wordDiv.appendChild(span);
 }
 
 function keydownAction(event) {
@@ -41,8 +61,10 @@ function keydownAction(event) {
   document.querySelector(".letter");
 }
 
+// for (i = 0; i < randomWord; i++);
+
 //loop through the word
-//creat a span with class letter-holder
+//create a span with class letter-holder
 //add an attribute "data-letter-val" and give it the value of the current letter in loop.
 //then attach the spans to your html
 
